d9f6972a54abfaeb6a05dbdd153ce3e4fdc71dd0
Jesse Zinn <jesserzinn@gmail.com>
2025-02-07 13:24:47 -0700

    Figuring out animation controller
    Trying to figure out the animation controller

---
+++ Adventure_Game/Assets/Scenes/Scripting 5/CharacterAnimatorController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimatorController : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        AnimatorHandler();
+    }
+    private void AnimatorHandler()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            animator.SetTrigger("RunTrigger");
+        }
+        else
+        {
+            animator.SetTrigger("IdleTrigger");
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("JumpTrigger");
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("DoubleJumpTrigger");
+        }
+    }
+}
