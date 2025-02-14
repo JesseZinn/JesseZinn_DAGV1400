dd5456368312cbd9dc8d22c3b4cfe46f0221303d
Jesse Zinn <jesserzinn@gmail.com>
2025-02-13 22:59:49 -0700

    Lab/Challenge 5
    I attempted to set up the animation controller but things were just not working. I also tried updating the character controller and the jump function was not doing its job.

--- Adventure_Game/Assets/Scenes/Scripting 4/SimpleCharacterMovement.cs
+++ Adventure_Game/Assets/Scenes/Scripting 4/SimpleCharacterMovement.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class SimpleCharacterMovement : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpForce;
+    public float gravity = -9.81f;
+
     private CharacterController controller;
-    private Vector3 movementVector;
+    private Vector3 velocity;
 
     void Start()
     {
@@ -16,14 +19,34 @@ public class SimpleCharacterMovement : MonoBehaviour
     void Update()
     {
         CharacterMovement();
+        ApplyGravity();
         KeepCharacterOnXAxis();
     }
 
     private void CharacterMovement()
     {
-        movementVector.x = Input.GetAxisRaw("Horizontal");
-        movementVector *= moveSpeed * Time.deltaTime;
-        controller.Move(movementVector);
+        float moveInput = Input.GetAxis("Horizontal");
+        Vector3 move = new Vector3(moveInput, 0, 0) * (moveSpeed * Time.deltaTime);
+        controller.Move(move);
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (!controller.isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        else
+        {
+            velocity.y = 0f;
+        }
+
+        controller.Move(velocity * Time.deltaTime);
     }
 
     private void KeepCharacterOnXAxis()
